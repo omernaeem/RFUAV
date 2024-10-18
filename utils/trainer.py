6303652eb94aa6df20f4396957a74671739b1a8f
frank <2403329962@qq.com>
2024-10-18 22:53:58 +0800

    intialize the trainer

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,3 +1,49 @@
 """
 定制训练器，由于使用默认的torch框架做。精度只有fp16
 """
+class trainer():
+   def __init__(self,
+                model,
+                train_loader,
+                val_loader,
+                criterion,
+                optimizer,
+                scheduler,
+                device,
+                num_epochs,
+                num_class,
+                save_path):
+      self.model = model
+      self.train_loader = train_loader
+      self.val_loader = val_loader
+      self.criterion = criterion
+      self.optimizer = optimizer
+      self.scheduler = scheduler
+      self.device = device
+      self.num_epochs = num_epochs
+      self.num_class = num_class
+      self.save_path = save_path
+      self.best_acc = 0
+      self.best_loss = 1e6
+      self.best_epoch = 0
+      self.best_model = None
+      
+   def set_up(self):
+      """
+      set up the model
+      """
+
+   def train(self):
+      """
+      train model
+      """
+      
+   def val(self):
+      """
+      val model
+      """
+      
+   def save_model(self):
+      """
+      save model
+      """
