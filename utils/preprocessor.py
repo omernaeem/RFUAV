3db2608ecadcf8e6bc3d6691d87353acbd1a3dee
frank <2403329962@qq.com>
2024-10-30 16:39:48 +0800

    加入了数据增强的函数

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,3 +1,18 @@
+"""模型预处理方法:
+1.给dataloader的预处理器 a.输入模型数据进行符合尺度的裁剪
+2.数据集用的数据增强方法 对图像数据进行:a.
+3.原始数据的处理方法 a.加噪 b.复杂环境融合 c.滤波
 """
-模型预处理方法，看dataloader里面写的多不多，多的话就分一下
+
+import numpy as np
+import cv2
+import albumentations
+
+
+"""Augmentations for image data augmentation
+All the augmentations methods are provided by albumentations: https://github.com/albumentations-team/albumentations
+
 """
+
+image = cv2.imread("")
+
