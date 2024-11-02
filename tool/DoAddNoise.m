db51efbe4e971d78593e7428904de01dfc3a96d3
Mengsy0823 <mengsy211007@163.com>
2024-11-02 09:31:16 +0800

    修正正确的噪声添加

--- tool/DoAddNoise.m
+++ tool/DoAddNoise.m
@@ -1,25 +1,40 @@
-function [outputArg1,outputArg2] = DoAddNoise(file_in,fileName,dataIQ,signal_power,noisevalue,target)
+function [outputArg1,outputArg2] = DoAddNoise(file_in,fileName,signal_power,noisevalue,target)
 %   此处显示详细说明
     targetSNR = 10^(noisevalue / 10); % 将 dB 转换为线性比例
     % 计算所需的噪声功率
     noisePower = signal_power / targetSNR; % 计算噪声功率
     
-    % 生成复数白噪声
-    noise = sqrt(noisePower/2) .* (randn(size(dataIQ)) + 1j * randn(size(dataIQ))); % 生成复数白噪声
-    noisy_data = dataIQ + noise;
-    
-    % 保存
+    file = fullfile(file_in,fileName);
+    fileInfo = dir(file); % 替换为你的文件名
+    fileSize = fileInfo.bytes; % 获取文件大小，单位为字节
+    times = fileSize /4/ 3e6;  
     filepathOut = file_in + '\' + fileName(1:end-4) + '-noise' ;
     if ~exist(filepathOut,"dir")
         mkdir(filepathOut);
     end
-    filepathOut = filepathOut + '\' + fileName(1:end-4) + '_' + num2str(target)+ "dB.dat";
-    fp = fopen(filepathOut,"wb+");
-    data=zeros(length(dataIQ)*2,1);
-    data(1:2:end) = real(noisy_data);
-    data(2:2:end) = imag(noisy_data);
-    fwrite(fp,data,"float");
-    fclose(fp);
-
+    filepathOut = filepathOut + '\' + num2str(target)+ "dB.dat";  
+    for i = 1:times
+        fp = fopen(file,"rb"); 
+        fseek(fp,3e6*(i-1),-1);
+        data = fread(fp,3e6,"float");
+        fclose(fp);
+        dataIQ = data(1:2:end) + 1j*data(2:2:end);
+        % 生成复数白噪声
+        noise = sqrt(noisePower/2) .* (randn(size(dataIQ)) + 1j * randn(size(dataIQ))); % 生成复数白噪声
+        noisy_data = dataIQ + noise;
+        
+%     datanoise = awgn1(dataIQ,noise);
+        % 保存  
+        if(i == 1)
+            fp = fopen(filepathOut,"wb+");
+        else
+            fp = fopen(filepathOut,"ab+");
+        end
+        data=zeros(length(dataIQ)*2,1);
+        data(1:2:end) = real(noisy_data);
+        data(2:2:end) = imag(noisy_data);
+        fwrite(fp,data,"float");
+        fclose(fp);
+    end
 end
 
---
+++ tool/snr_esti_demo.m
@@ -0,0 +1,68 @@
+clc;
+clear;
+close all;
+
+%% Parameter Init
+NFFT = 204800;
+data_len = 1.5e6;    % 读取点数
+ddc_decifactor = 4; % 下变频降采样指数
+fs = 100e6;
+
+%% 下面三个是一定要修改的参数：信号带宽、文件名字、文件路径
+band_width = 40e6;  % 信号带宽
+path = "E:\DataBase\DJFPVCOMBO-22db-90db_5760m_100m_40m\DJFPVCOMBO-22db-90db_5760m_100m_40m_6-8s-noise";    % 文件路径
+file = "10dB.dat";                              % 文件名字
+
+%% Load data
+fp = fopen(fullfile(path, file), "rb");
+data = fread(fp, data_len * 2, "float");
+dataIQ = data(1:2:end) + data(2:2:end) * 1j;
+fclose(fp);
+dataIQ = normalize(dataIQ, "norm");
+
+%% 确定频偏与降采样
+subplot(311)
+[pxx, f] = pwelch(dataIQ, hamming(round(length(dataIQ) / 10)), [], NFFT, fs, "centered");
+plot(f, db(pxx))                % 信号功率谱
+title("原始信号的PSD")
+[~, max_index] = max(db(pxx(500:end-500)));
+freq_shift = f(max_index+500);    % 频偏
+
+
+%% 下变频
+tvec = (1:length(dataIQ))' / fs;
+carreir = exp(1j*2*pi*-freq_shift.*tvec);
+data_baseband = dataIQ .* carreir;
+data_resmaple = resample(data_baseband, fs/2, fs);
+fs = fs/2;
+
+
+%% 绘图
+subplot(312)
+pwelch(data_resmaple, hamming(round(length(data_resmaple) / 10)), [], NFFT, fs, "centered");
+title("下变频-降采样处理后信号功率谱")
+
+
+subplot(313)
+temp = abs(fftshift(fft(data_resmaple, NFFT) / NFFT));
+plot((temp));
+axis tight
+
+index1 = round(NFFT/2 - band_width * 0.75 / 2 / fs * NFFT);
+index2 = round(NFFT/2 + band_width * 0.75 / 2 / fs * NFFT);
+xline(index1, "r");
+xline(index2, "r");
+signal_power = mean(temp(index1 : index2) .^ 2);     % 信号功率
+
+index11 = round(NFFT/2 - (band_width * 1.1 + 2.5e6) / 2 / fs * NFFT);
+index22 = round(NFFT/2 - band_width  * 1.0 / 2 / fs * NFFT);
+
+xline(index11, "k");
+xline(index22, "k");
+title("红色区域内为截取的信号，黑色区域内为截取的噪声")
+noise_pwoer = mean(temp(index11 : index22) .^ 2);    % 噪声功率
+
+
+%% SNR Esti
+snr_esti = 10*log10((signal_power - noise_pwoer) / noise_pwoer);
+fprintf("SNR估计值为: %.2fdb\n", snr_esti);
