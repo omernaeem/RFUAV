5242f93f40f82fa4eceb0fcd59ee35c9ff461d10
server <2403329962@qq.com>
2024-11-13 11:15:10 +0800

    新增计算评估函数的统一类

--- utils/metrics/base_metric.py
+++ utils/metrics/base_metric.py
@@ -1,5 +1,14 @@
+"""
+模型评估的基础参数和最大接口
+---------ToDo
+加logger和注释
+"""
+
 from abc import ABCMeta, abstractmethod
 from logging import Logger
+from typing import Union, Sequence
+import torch
+from precision import MultiLabelMixin
 from typing import Any, Dict, List, Optional
 
 
@@ -82,3 +91,52 @@ class BaseMetric(metaclass=ABCMeta):
 
         The returned result of the metric compute should be a dictionary.
         """
+
+
+class EVAlMetric(MultiLabelMixin, BaseMetric):
+    """Wrapper to get different task of PrecisionRecallF1score calculation, by
+    setting the ``task`` argument to either ``'singlelabel'`` or
+    ``multilabel``.
+
+    See the documentation of :mod:`SingleLabelPrecisionRecallF1score` and
+    :mod:`MultiLabelPrecisionRecallF1score` for the detailed usages and
+    examples.
+    """
+
+    def __new__(cls,
+                task: str = 'singlelabel',
+                num_classes: Optional[int] = None,
+                thrs: Union[float, Sequence[Optional[float]], None] = None,
+                topk: Optional[int] = None,
+                items: Sequence[str] = ('precision', 'recall', 'f1-score'),
+                average: Optional[str] = 'macro',
+                **kwargs):
+
+        assert isinstance(thrs, float) or thrs is None, \
+            "task `'multilabel'` only supports single threshold or None."
+        assert isinstance(num_classes, int), \
+            '`num_classes` is necessary for multi-label metrics.'
+        return MultiLabelPrecisionRecallF1score(
+            num_classes=num_classes,
+            thr=thrs,
+            topk=topk,
+            items=items,
+            average=average,
+            **kwargs)
+
+
+# Usage-------------------------------------
+def main():
+
+    preds = torch.tensor([2, 0, 1, 1])
+    labels = torch.tensor([2, 1, 2, 0])
+    metric = EVAlMetric(num_classes=3)
+    metric(preds, labels)
+    # {'precision': 33.3333, 'recall': 16.6667, 'f1-score': 22.2222}
+    metric = EVAlMetric(task="multilabel", average='micro', num_classes=3)
+    metric(preds, labels)
+    # {'precision_micro': 25.0, 'recall_micro': 25.0, 'f1-score_micro': 25.0}
+
+
+if __name__ == '__main__':
+    main()
