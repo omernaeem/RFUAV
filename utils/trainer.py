024e77871f9adf8947a0cafbbba45c353b0a8e05
server <2403329962@qq.com>
2024-12-04 22:14:22 +0800

    update the yolo train method

--- utils/trainer.py
+++ utils/trainer.py
@@ -14,9 +14,10 @@ from utils.logger import colorful_logger
 import cv2
 from abc import abstractmethod
 from .metrics.base_metric import EVAMetric
-
 import sys
-import argparse
+
+
+from tqdm import tqdm
 from pathlib import Path
 from utils.DetModels.yolo import DetectionModel
 import numpy as np
@@ -30,20 +31,19 @@ from utils.DetModels.yolo.general import (check_file, check_yaml, yaml_save,init
                                           check_suffix, check_amp, check_img_size, labels_to_class_weights, labels_to_image_weights)
 from utils.DetModels.yolo.basic import increment_path, colorstr
 from utils.DetModels.yolo.torch_utils import (select_device, torch_distributed_zero_first,
-                                              smart_optimizer, de_parallel, EarlyStopping)
+                                              smart_optimizer, de_parallel, EarlyStopping, ModelEMA)
 from utils.DetModels.yolo.dataloader import create_dataloader
 from utils.DetModels.yolo.autoanchor import check_anchors
 from utils.DetModels.yolo.callbacks import Callbacks
 from utils.DetModels.yolo.loss import ComputeLoss
 import utils.DetModels.yolo.val as validate
-import utils.DetModels.yolo.metrics as fitness
-
+from utils.DetModels.yolo.metrics import fitness
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 METRIC = os.path.join(current_dir, './metrics')
 sys.path.append(METRIC)
-
+TQDM_BAR_FORMAT = '{l_bar}{bar:10}{r_bar}'
 
 class Basetrainer():
 
@@ -429,10 +429,12 @@ class CustomTrainer(Basetrainer):
             self.save_model(metrics, epoch)
 
 
-class DetTrainer():
+# for test--------------------------------------------------------------------------------------------------------------
+class DetTrainer:
     def __int__(self):
         pass
     def train(self, hyp, opt, callbacks, device):
+
         save_dir, epochs, batch_size, weights, single_cls, evolve, data, cfg, resume, noval, nosave, workers, freeze = \
             Path(opt.save_dir), opt.epochs, opt.batch_size, opt.weights, opt.single_cls, opt.evolve, opt.data, opt.cfg, \
                 opt.resume, opt.noval, opt.nosave, opt.workers, opt.freeze
@@ -455,20 +457,15 @@ class DetTrainer():
             yaml_save(save_dir / 'opt.yaml', vars(opt))
 
         # Config
-        plots = not evolve and not opt.noplots  # create plots
-        cuda = device.type != 'cpu'
-
         init_seeds(opt.seed + 1, deterministic=True)
         with torch_distributed_zero_first(-1):
             data_dict = check_dataset(data)  # check if None
         train_path, val_path = data_dict['train'], data_dict['val']
         nc = 1 if single_cls else int(data_dict['nc'])  # number of classes
         names = {0: 'item'} if single_cls and len(data_dict['names']) != 1 else data_dict['names']  # class names
-        is_coco = isinstance(val_path, str) and val_path.endswith('coco/val2017.txt')  # COCO dataset
 
         # Model
         check_suffix(weights, '.pt')  # check weights
-        pretrained = weights.endswith('.pt')
 
         model = DetectionModel(cfg, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
         amp = check_amp(model)  # check AMP
@@ -486,7 +483,6 @@ class DetTrainer():
         gs = max(int(model.stride.max()), 32)  # grid size (max stride)
         imgsz = check_img_size(opt.imgsz, gs, floor=gs * 2)  # verify imgsz is gs-multiple
 
-
         # Optimizer
         nbs = 64  # nominal batch size
         accumulate = max(round(nbs / batch_size), 1)  # accumulate loss before optimizing
@@ -495,13 +491,11 @@ class DetTrainer():
 
         # Scheduler
         lf = lambda x: (1 - x / epochs) * (1.0 - hyp['lrf']) + hyp['lrf']  # linear
-        scheduler = lr_scheduler.LambdaLR(optimizer,
-                                          lr_lambda=lf)  # plot_lr_scheduler(optimizer, scheduler, epochs)
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)  # plot_lr_scheduler(optimizer, scheduler, epochs)
         # EMA
-        ema = None
+        ema = ModelEMA(model)
         # Resume
         best_fitness, start_epoch = 0.0, 0
-
 
         # Trainloader
         train_loader, dataset = create_dataloader(train_path,
@@ -575,8 +569,7 @@ class DetTrainer():
                     f"Logging results to {colorstr('bold', save_dir)}\n"
                     f'Starting training for {epochs} epochs...')
 
-        for epoch in range(start_epoch,
-                           epochs):  # epoch ------------------------------------------------------------------
+        for epoch in range(start_epoch, epochs):  # epoch --------------------------------------------------------------
             callbacks.run('on_train_epoch_start')
             model.train()
 
@@ -595,6 +588,7 @@ class DetTrainer():
             pbar = enumerate(train_loader)
             print(
                 ('\n' + '%11s' * 7) % ('Epoch', 'GPU_mem', 'box_loss', 'obj_loss', 'cls_loss', 'Instances', 'Size'))
+            pbar = tqdm(pbar, total=nb, bar_format=TQDM_BAR_FORMAT)  # progress bar
 
             optimizer.zero_grad()
             for i, (
@@ -724,90 +718,9 @@ class DetTrainer():
     def dataloader(self):
         pass
     def save_model(self, metrics, epoch):
-        """
-        Save the model checkpoint.
-        """
+
         self.logger.log_with_color(f"Saving model at epoch {epoch}")
         torch.save(self.model.state_dict(), os.path.join(self.save_path, f'{epoch}.pth'))
-
-# for test--------------------------------------------------------------------------------------------------------------
-
-
-def parse_opt(known=False):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=None, help='initial weights path')
-    parser.add_argument('--cfg', type=str, default=, help='models/yolov5n.yaml')
-    parser.add_argument('--data', type=str, default=, help='dataset.yaml path')
-    parser.add_argument('--hyp', type=str, default=, help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=10, help='total training epochs')
-    parser.add_argument('--batch-size', type=int, default=32, help='total batch size for all GPUs, -1 for autobatch')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
-    parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
-    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
-    parser.add_argument('--noval', action='store_true', help='only validate final epoch')
-    parser.add_argument('--noautoanchor', action='store_true', help='disable AutoAnchor')
-    parser.add_argument('--noplots', action='store_true', help='save no plot files')
-    parser.add_argument('--evolve', type=int, nargs='?', const=300, help='evolve hyperparameters for x generations')
-    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
-    parser.add_argument('--cache', type=str, nargs='?', const='ram', help='image --cache ram/disk')
-    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
-    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
-    parser.add_argument('--single-cls', action='store_true', help='train multi-class data as single-class')
-    parser.add_argument('--optimizer', type=str, choices=['SGD', 'Adam', 'AdamW'], default='SGD', help='optimizer')
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument('--workers', type=int, default=8, help='max dataloader workers (per RANK in DDP mode)')
-    parser.add_argument('--project', default=, help='save to project/name')
-    parser.add_argument('--name', default='exp', help='save to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--quad', action='store_true', help='quad dataloader')
-    parser.add_argument('--cos-lr', action='store_true', help='cosine LR scheduler')
-    parser.add_argument('--label-smoothing', type=float, default=0.0, help='Label smoothing epsilon')
-    parser.add_argument('--patience', type=int, default=100, help='EarlyStopping patience (epochs without improvement)')
-    parser.add_argument('--freeze', nargs='+', type=int, default=[0], help='Freeze layers: backbone=10, first3=0 1 2')
-    parser.add_argument('--save-period', type=int, default=-1, help='Save checkpoint every x epochs (disabled if < 1)')
-    parser.add_argument('--seed', type=int, default=0, help='Global training seed')
-    parser.add_argument('--local_rank', type=int, default=-1, help='Automatic DDP Multi-GPU argument, do not modify')
-    # Logger arguments
-    parser.add_argument('--entity', default=None, help='Entity')
-    parser.add_argument('--upload_dataset', nargs='?', const=True, default=False, help='Upload data, "val" option')
-    parser.add_argument('--bbox_interval', type=int, default=-1, help='Set bounding-box image logging interval')
-    parser.add_argument('--artifact_alias', type=str, default='latest', help='Version of dataset artifact to use')
-
-    return parser.parse_known_args()[0] if known else parser.parse_args()
-
-
-def main(opt, callback = Callbacks()):
-
-    opt.data, opt.hyp, opt.weights, opt.project = \
-        check_file(opt.data), check_yaml(opt.hyp), str(opt.weights), str(opt.project)  # checks
-    opt.cfg = check_yaml(opt.cfg)
-    assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
-
-    opt.save_dir = ''
-
-    device = select_device(opt.device, batch_size=opt.batch_size)
-    test = DetTrainer()
-    # Train
-    if not opt.evolve:
-        DetTrainer.train(opt.hyp, opt, device, callback)
-
-
-def run(**kwargs):
-    # Usage: import train; train.run(data='coco128.yaml', imgsz=320, weights='yolov5m.pt')
-    opt = parse_opt(True)
-    for k, v in kwargs.items():
-        setattr(opt, k, v)
-    main(opt)
-    return opt
-
-
-if __name__ == '__main__':
-    opt = parse_opt()
-    main(opt)
-
-
 
 
 def show_img_in_dataloader(images):
